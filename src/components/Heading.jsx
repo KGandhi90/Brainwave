c50f5b2f2b3e4bb058b8b9664dad6aed3ac67f56
KGandhi90 <kevin.gandhi@somaiya.edu>
2025-02-03 22:47:13 +0530

    Collaboration Section Completed

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Heading = ({ classname, title }) => {
     return(
         <div className={`${classname} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>{title && <h2 className='h2'>{title}</h2>}</div>
